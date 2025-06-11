0eded6d620fa1d53a5874d592d1ab1b628ab9de6
Ihor Burchik <burchik555@gmail.com>
2025-06-11 18:53:59 +0300

    Comments1

--- static/js/main.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll('.save-scroll').forEach(button => {
-    button.addEventListener('click', () => {
-        sessionStorage.setItem('scrollPosition', window.scrollY);
-    });
-});
